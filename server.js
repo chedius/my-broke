ea14ebab078d4d80595f9f184751f1f924a758bc
chedius <chedius232@google.com>
2019-10-21 18:16:23 +0600

    adding webpack auto-build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 8080;
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/my-broke'));
@@ -13,3 +14,4 @@ res.sendFile(path.join(__dirname+'/dist/my-broke/index.html'));
  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log(`server is listening on ${port}`);
