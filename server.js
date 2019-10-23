7a7161f32c0bcca9d39c7bad4880206765b38bc1
chedius <chedius232@google.com>
2019-10-23 20:19:45 +0600

    webpack ok

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my-broke'));
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Vadim:123@mybroke-4ojdc.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-app.get('/*', function(req,res) {
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('/', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/my-broke/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 console.log(`server is listening on ${port}`);
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // плагин минимизации
+const CopyPlugin = require('copy-webpack-plugin');
+
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // плагин минимизации
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
+   // context: __dirname + '/app',
     entry: {
         'polyfills': './src/polyfills.ts',
-        'app': './src/main.ts'
+        'app': './src/main.ts',
+   //   'vendor': ['angular']
       },
    output:{
        path: path.resolve(__dirname, 'dist'),     // путь к каталогу выходных файлов - папка public
-       publicPath: '/',
+       publicPath: '',
        filename: '[name].[hash].js'
    },
    devServer: {
@@ -36,21 +40,23 @@ module.exports = {
             },{
             test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
             loader: 'file-loader?name=assets/[name].[hash].[ext]'
-          },{
+          }, {
             test: /\.css$/,
-            exclude: path.resolve(__dirname, 'src/app'),
+           use: ['to-string-loader', 'css-loader'],
+          },{
+            test: /\.(png|svg|jpg|gif|jpeg)$/,
             use: [
-              MiniCssExtractPlugin.loader,
-              "css-loader"
-            ]
-          },{
-            test: /\.css$/,
-            include: path.resolve(__dirname, 'src/app'),
-            loader: 'raw-loader'
-          }
+                'file-loader',
+            ],
+          
+          },
        ]
    },
+   
    plugins: [
+     new CopyPlugin ([
+       {from: "src/assets", to: "assets"}
+     ]),
     new webpack.ContextReplacementPlugin(
         /angular(\\|\/)core/,
         path.resolve(__dirname, 'src'), // каталог с исходными файлами
@@ -62,7 +68,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css"
     }),
-    new UglifyJSPlugin(),
+    //new UglifyJSPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.LoaderOptionsPlugin({
       htmlLoader: {
